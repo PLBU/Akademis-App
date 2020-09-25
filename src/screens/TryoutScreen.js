0540bc9ea05051ca207068a43f6a9e3cc74c2494
Renaldi Arlin <johanes123renaldi@gmail.com>
2020-09-26 01:06:58 +0700

    Integrated tryout Catalogue API

--- src/screens/TryoutScreen.js
+++ src/screens/TryoutScreen.js
@@ -18,6 +18,8 @@ import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import { RFValue } from "react-native-responsive-fontsize";
 import axios from 'react-native-axios';
+import Accordion from 'react-native-collapsible/Accordion';
+import { Overlay } from 'react-native-elements';
 
 //Styles
 import styles from '../styles/mainScreenStyle.js';
@@ -31,6 +33,7 @@ import notFoundImage from '../assets/images/image-not-found-bg-terang.png'
 import saintekSingleImage from '../assets/images/saintek-single.png'
 import soshumSingleImage from '../assets/images/soshum-single.png'
 import purchaseToImage from '../assets/images/purchase-to-bg-gelap.png'
+import buySuccessImage from '../assets/images/pembelian-sukses-bg-terang.png'
 
 const items = [
   {
@@ -243,7 +246,12 @@ export default [
   },
   //Details Tryout
   ({route, navigation}) => {
-    const { id, name, price, } = route.params
+    const { id, name, price, start_at, end_at } = route.params
+    const [tests, setTests] = React.useState([])
+    const [subTests, setSubTests] = React.useState([[], []])
+    const [activeSections, setActiveSections] = React.useState([])
+    const [buySuccessModal, setBuySuccessModal] = React.useState(false)
+
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -251,8 +259,85 @@ export default [
 
     today = dd + '/' + mm + '/' + yyyy;
 
+    const getData = () => {
+      axios.get(`https://dev.akademis.id/api/tryout/${id}`)
+        .then(res => {
+          var arr = res.data.data.test
+          var newArr = []
+
+          arr.forEach(element => {
+            newArr.push(element.subtest)
+          })
+
+          setTests(arr)
+          setSubTests(newArr)
+
+          console.log("Data response: ")
+          console.log(arr)
+          console.log("Subtest response: ")
+          console.log(newArr)
+        })
+        .catch(e => console.log(e) )
+    }
+
+    const _renderHeader = sections => (
+      <View style={{width: RFValue(320), padding: RFValue(10), backgroundColor: theme.PRIMARY_DARK_COLOR, justifyContent: 'center', alignItems: 'center', borderColor: theme.SECONDARY_DARK_COLOR, borderWidth: 0.5}}>
+        <Text style={{fontSize: RFValue(17), color: 'white'}}>{sections.name}</Text>
+      </View>
+    )
+
+    const _renderContent = (sections, index) => (
+        subTests[index].map( (value) => (
+          <View style={{marginVertical: 10, justifyContent: 'center', alignItems: 'center', borderColor: theme.SECONDARY_DARK_COLOR, borderRadius: 15, borderWidth: 0.5, padding: 5}}>
+            <Text style={{fontSize: RFValue(15) }}>{value.name}</Text>
+            <Text style={{fontSize: RFValue(14), color: 'gray' }}>Waktu : {value.time} menit</Text>
+          </View>
+        ) )
+    )
+
+    React.useEffect(() => {
+      getData()
+    }, [])
+
     return (
       <ScrollView contentContainerStyle={[{flexGrow: 1}, styles.bgAll]}>
+        {/* Modal pembelian berhasil */}
+        <Overlay
+          animationType="fade"
+          fullscreen={true}
+          isVisible={buySuccessModal}
+          onRequestClose={() => {
+            setBuySuccessModal(false)
+          }}
+          overlayStyle={styles.overlay}
+        >
+          <View style={styles.centeredView}>
+            <View style={{
+              width: Dimensions.get('window').width*0.8, 
+              backgroundColor: 'white',
+              elevation: 5,
+              alignItems: 'center',
+              margin: 20,
+              borderRadius: 25,
+              overflow: 'hidden',
+              }}
+            >
+              <View style={{backgroundColor: theme.PRIMARY_DARK_COLOR, height: 50, width: '100%'}}>
+                <Text style={[styles.mediumWhiteText, {margin: 10, left: 15}]}>Pembelian berhasil!</Text>
+              </View>
+              <View style={{ width: Dimensions.get('window').width*0.8, alignItems: 'center'}}>
+                <Image source={buySuccessImage} style={{width: RFValue(200), height: RFValue(150) }}/>
+                <TouchableOpacity 
+                  style={[styles.button, {width: RFValue(270) } ]} 
+                  onPress={() => {setBuySuccessModal(false), navigation.goBack() } }
+                >
+                  <Text style={styles.buttonText}>Oke mantap</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </Overlay>
+
         <View style={{
           width: Dimensions.get('window').width*0.7, 
           height: Dimensions.get('window').width*0.7*0.75,
@@ -284,24 +369,38 @@ export default [
             <Text style={styles.leftMediumText}>Harga : {price}{"   "}</Text>
             <Image source={diamond} style={{width: 22, height: 22}}/>
           </View>
+          <Text style={styles.leftMediumText}>Waktu Tryout : {"\n"}
+            <Text style={styles.leftSmallText}>{start_at}{"\n"}hingga{"\n"}{end_at}</Text>
+          </Text>
           <Text style={styles.leftMediumText}>Tanggal Pembelian : {"\n"}
             <Text style={styles.leftSmallText}>{today}</Text>
           </Text>
         </View>
+
+        <Text style={{left: 20, fontSize: 22, marginTop: 30}}>Konten Tryout</Text>
+        <View style={styles.horizontalRuler}/>
+
+        <Accordion
+          sections={tests}
+          activeSections={activeSections}
+          renderHeader={_renderHeader}
+          renderContent={_renderContent}
+          onChange={ active => setActiveSections(active) }
+          underlayColor={theme.PRIMARY_ACCENT_COLOR}
+          containerStyle={{width: RFValue(320), alignSelf: 'center', borderRadius: 20, overflow: 'hidden', marginTop: RFValue(10)}}
+        />
 
         <Text style={{left: 20, fontSize: 22, marginTop: 30}}>Metode Pembayaran</Text>
         <View style={styles.horizontalRuler}/>
 
         <View style={{margin: 20, alignItems: 'center'}}>
           <TouchableOpacity style={styles.button} onPress={ () => {
-              Alert.alert("Sukses", "Pembayaran berhasil")
-              navigation.goBack()
+              setBuySuccessModal(true)
           }}>
             <Text style={styles.buttonText}>Beli dengan diamond</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={ () => {
-              Alert.alert("Sukses", "Pembayaran berhasil")
-              navigation.goBack()
+              setBuySuccessModal(true)
           }}>
             <Text style={styles.buttonText}>Beli via share ke media sosial</Text>
           </TouchableOpacity>
